72f0e3a0ad6ba7aa97dbc291893b0df954ca3d55
Sayan Paul <sayanp607@gmail.com>
2025-07-11 21:16:41 +0530

    socket fixed

--- socket/socket.js
+++ socket/socket.js
@@ -6,12 +6,13 @@ const app = express();
 
 const server = http.createServer(app);
 
-const io = new Server (server, {
-  cors:{
-    origin:'http://localhost:5173',
-    methods:['GET','POST']
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:5173', 'https://insta-frontend-ehjp.vercel.app'],
+    methods: ['GET', 'POST'],
+    credentials: true,
   }
-})
+});
 
 const userSocketMap = {} ;// this map stores socket id corresponding the user id; userId -> socketId
 
